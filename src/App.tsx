f84ce0fb128ece1418150849bd46b928c11cf693
İlayda Sena Keleş <ilaydasenakeles28@gmail.com>
2023-03-10 18:19:28 +0300

    Added searchBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
 import React from "react";
-import { Text, View, SafeAreaView, FlatList, StyleSheet } from "react-native";
+import { Text, View, SafeAreaView, FlatList, StyleSheet, TextInput} from "react-native";
 import PatikaStore_data from "./PatikaStore_data.json";
 import ProductsCard from "./Components/ProductsCard";
 
 function App () {
 
 return(
-  <SafeAreaView style={ styles.container }>
+  <SafeAreaView style={ styles.container } >
     <View>
     <Text style={styles.header}>PATIKASTORE</Text>
-
+    <TextInput style={styles.searchBar} placeholder= { '   Ara...'}></TextInput>
       <FlatList 
       numColumns = {2}
       data={PatikaStore_data}
       renderItem= {({item}) => <ProductsCard product = {item} /> }
       />
-
+      
     </View>
   </SafeAreaView>
 
@@ -32,8 +32,15 @@ const styles= StyleSheet.create ({
     fontSize:40,
     color: 'purple',
     fontWeight: 'bold',
-    margin: 10,
+    marginLeft: 10,
+    marginTop:5,
   },
+  searchBar:{
+    backgroundColor:'#eceff1',
+    margin: 10,
+    borderRadius:15,
+
+  }
 })
 
 export default App
