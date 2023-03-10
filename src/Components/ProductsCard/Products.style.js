9d9b20e7f6d9f47014446b0e33c8a20f24125de4
İlayda Sena Keleş <ilaydasenakeles28@gmail.com>
2023-03-10 17:59:40 +0300

    Created Styles

--- src/Components/ProductsCard/Products.style.js
+++ src/Components/ProductsCard/Products.style.js
@@ -1,5 +1,32 @@
 import { Dimensions, StyleSheet } from "react-native";
 
 export default StyleSheet.create ({
+    container:{
+        backgroundColor: '#eceff1',
+        borderRadius:10,
+        margin:10,
+        flex:1,
+    },
+
+    Image:{
+        height: Dimensions.get( 'window' ).height/4, margin:10,
+        borderRadius:10,
+
+    },
+    title:{
+        fontSize: 18,
+        fontWeight:'bold',
+        color:'black',
+        paddingLeft:10,
+        paddingBottom:5,
+    },
+    price:{
+        fontSize:15,
+        fontWeight:'bold',
+        color:'gray',
+        paddingLeft:10,
+        paddingBottom:10,
+    },
     
+
 })
