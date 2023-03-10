71f2a9825a9f746d785aaa93421bcd2556161033
İlayda Sena Keleş <ilaydasenakeles28@gmail.com>
2023-03-10 17:57:35 +0300

    Created Component and FlatList

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import React from "react";
-import { Text, View, SafeAreaView, FlatList } from "react-native/types";
+import { Text, View, SafeAreaView, FlatList, StyleSheet } from "react-native";
 import PatikaStore_data from "./PatikaStore_data.json";
+import ProductsCard from "./Components/ProductsCard";
 
 function App () {
-return(
-  <SafeAreaView>
-    <View>
 
-      <FlatList
-      data={[]}
-      renderItem= {({item}) => <Text> {item}</Text>}
-      
+return(
+  <SafeAreaView style={ styles.container }>
+    <View>
+    <Text style={styles.header}>PATIKASTORE</Text>
+
+      <FlatList 
+      numColumns = {2}
+      data={PatikaStore_data}
+      renderItem= {({item}) => <ProductsCard product = {item} /> }
       />
 
     </View>
@@ -19,5 +22,18 @@ return(
 );
 
 }
+
+const styles= StyleSheet.create ({
+  container:{
+    flex:1,
+    backgroundColor: 'white'
+  },
+  header:{
+    fontSize:40,
+    color: 'purple',
+    fontWeight: 'bold',
+    margin: 10,
+  },
+})
 
 export default App
---
+++ src/Components/ProductsCard/Products.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Text, View, Image } from "react-native";
+import styles from './Products.style'
+
+const product = ({product}) => {
+    return(
+        <View style={styles.container}>
+            <Image style={styles.Image} source={{ uri: product.imgURL}}/>
+            <Text style={styles.title}> { product.title } </Text>
+            <Text style={styles.price}> { product.price }</Text>
+            
+        </View>
+    )
+
+}
+
+
+export default product;
